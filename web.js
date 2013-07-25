756526b20bbd561ab74ccf7af54b31c00a6dcf39
Dioni Zhong <dioniys@yahoo.com>
2013-07-25 08:46:34 +0100

    crowdfunding front page

--- web.js
+++ web.js
@@ -1,3 +1,8 @@
+// sudo ipfw -q add 10 allow tcp from any to any 8080 in
+// sudo ipfw -q add 20 allow tcp from any to any 8080 out
+// sudo ipfw show
+// ifconfig // get en0: inet
+
 var express = require('express');
 var fs = require('fs');
 
@@ -9,7 +14,7 @@ app.get('/', function(request, response) {
   	response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
